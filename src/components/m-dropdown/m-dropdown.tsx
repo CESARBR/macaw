8616f909d00e7d782a59a6af988ec10a0b365d3e
alissonrgalindo <alissonricardogalindo@gmail.com>
2020-08-27 14:13:10 -0300

    Adjustments UI

--- src/components/m-dropdown/m-dropdown.tsx
+++ src/components/m-dropdown/m-dropdown.tsx
@@ -3,7 +3,7 @@ import { Component, Prop, State, h, Event } from '@stencil/core';
 @Component({
   tag: 'm-dropdown',
   styleUrl: 'm-dropdown.scss',
-  shadow: true,
+  shadow: false,
 })
 export class Dropdown {
 
@@ -22,7 +22,7 @@ export class Dropdown {
     return (
       <div class="dropdown">
         <m-button onClick={(event: UIEvent) => this.handleClick(event)}>
-          {this.label} <span class={'icon ' + (this.open ? '' : 'icon--closed')}></span>
+          {this.label}  <m-icon name="chevron-bottom" class={'icon ' + (this.open ? '' : 'icon--closed')} />
         </m-button>
         <div class={'dropdown__content ' + (this.open ? 'dropdown__content--show' : '')}>
           <ul class="list-items" onClick={(event: UIEvent) => this.handleClick(event)}>
