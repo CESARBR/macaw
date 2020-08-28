a5434e40fb818363476fb7b5d0ac3d372ba00d6b
Bruno Lima <bhml@cesar.org.br>
2020-08-28 15:43:45 -0300

    Fix and bump version

--- src/components/m-dropdown/m-dropdown.tsx
+++ src/components/m-dropdown/m-dropdown.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, State, h, Event } from '@stencil/core';
+import { Component, Prop, State, h } from '@stencil/core';
 
 @Component({
   tag: 'm-dropdown',
@@ -14,18 +14,18 @@ export class Dropdown {
 
   @State() open: boolean;
 
-  handleClick(event: UIEvent) {
+  handleClick() {
     this.open = !this.open;
   }
 
   render() {
     return (
       <div class="m-dropdown">
-        <m-button onClick={(event: UIEvent) => this.handleClick(event)}>
+        <m-button onClick={() => this.handleClick()}>
           {this.label}  <m-icon name="chevron-bottom" class={'icon ' + (this.open ? '' : 'icon--closed')} />
         </m-button>
         <div class={'m-dropdown__content ' + (this.open ? 'm-dropdown__content--show' : '')}>
-          <ul class="list-items" onClick={(event: UIEvent) => this.handleClick(event)}>
+          <ul class="list-items" onClick={() => this.handleClick()}>
             <slot/>
           </ul>
         </div>
