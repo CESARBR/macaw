71b0d0f47f9b787f35de337e2f903b7522ca41fa
filipe-torres <fvvtb@cesar.org.br>
2020-11-19 21:17:28 +0000

    Componente Menu Apps

---
+++ src/components/m-menu-apps-item/m-menu-apps-item.tsx
@@ -0,0 +1,17 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'm-menu-apps-item',
+  styleUrl: 'm-menu-apps-item.scss',
+  shadow: false
+})
+
+export class MenuAppsItem {
+  render() {
+    return (
+      <li class="m-menu-apps-item">
+        <slot/>
+      </li>
+    )
+  }
+}
