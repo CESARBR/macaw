a43ab20b180be429b0e128f9b05ef6e523c4cb16
filipe-torres <fvvtb@cesar.org.br>
2020-11-19 21:17:28 +0000

    Componente Menu Apps

---
+++ src/components/m-menu-apps-item/readme.md
@@ -0,0 +1,32 @@
+# m-avatar
+
+This is a magic avatar or not ;)
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property  | Attribute | Description              | Type     | Default     |
+| --------- | --------- | ------------------------ | -------- | ----------- |
+| `altDesc` | `alt`     | Alternative description. | `string` | `undefined` |
+| `source`  | `source`  | Image URL or user name.  | `string` | `undefined` |
+
+
+## Dependencies
+
+### Used by
+
+ - [m-header](../m-header)
+
+### Graph
+```mermaid
+graph TD;
+  m-header --> m-avatar
+  style m-avatar fill:#f9f,stroke:#333,stroke-width:4px
+```
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
---
+++ src/components/m-menu-apps/m-menu-apps.tsx
@@ -0,0 +1,51 @@
+import { Component, h, Prop, State } from '@stencil/core';
+
+@Component({
+  tag: 'm-menu-apps',
+  styleUrl: 'm-menu-apps.scss',
+  shadow: false,
+  assetsDirs: ['icons']
+})
+
+export class MenuApps {
+
+  /**
+  * Alternative description.
+  */
+  @Prop({ attribute: 'alt' }) altDesc: string;
+
+  /**
+  * State of menu (opened or closed)
+  */
+  @State() open: boolean;
+
+  /**
+  * State of menu (opened or closed)
+  */
+  handleClick() {
+    this.open = !this.open;
+  }
+
+  /**
+   * Menu Item items data.
+   */
+  @Prop() menuItems: [];
+
+  render() {
+    return (
+      <div class="m-menu-apps" onClick={() => this.handleClick()}>
+        <button class="m-menu-apps__button">
+        <m-icon type="core" name="menu-apps-icon"></m-icon>
+        </button>
+        <div class={'m-menu-apps__content ' + (this.open ? 'm-menu-apps__content--show' : '')}  onClick={() => this.handleClick()}>
+          {this.menuItems ? 
+            this.menuItems.map((item, index) =>
+            <m-menu-apps-item innerHTML={item}></m-menu-apps-item>
+            )
+            : <slot />
+          }
+        </div>
+      </div>
+    )
+  }
+}
