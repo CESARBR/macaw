373c5ca590d5e5be9100b0953cbf399158bbeec0
filipe-torres <fvvtb@cesar.org.br>
2021-01-22 08:55:53 +0000

    1) Funções genéricas movidas para o arquivo utils.ts; 2) Remoção da função de validação de URL (duplicada)

--- src/components/m-menu-apps/m-menu-apps.tsx
+++ src/components/m-menu-apps/m-menu-apps.tsx
@@ -1,4 +1,5 @@
 import { Component, h, Prop, State } from '@stencil/core';
+import { isObject, isArray, isStringAndNotEmpty, validURL } from '../../utils/utils';
 
 @Component({
   tag: 'm-menu-apps',
@@ -33,33 +34,17 @@ export class MenuApps {
         mostUsedItems: Array<object>, commonItems: Array<object>, partnersItems: Array<object>;
 
     if(menuData) {
-      function isObject(objectItem: any) {
-        return objectItem.constructor === Object;
-      }
-
-      function isArray(arrayItem: any) {
-        return arrayItem instanceof Array;
-      }
-
-      function isStringValid(stringValue: any) {
-        return (typeof stringValue === 'string' && stringValue !== '');
-      }
-
-      function validateUrl(value: string) {
-        return (isStringValid(value) && /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value));
-      }
-
       function menuItemIsComplete(menuItem: any) {
         let AttributeNames = Object.keys(menuItem);
 
         return (AttributeNames.includes('link') && 
-                validateUrl(menuItem.link) && 
+                validURL(menuItem.link) && 
                 AttributeNames.includes('imgSrc') && 
-                validateUrl(menuItem.imgSrc) && 
+                validURL(menuItem.imgSrc) && 
                 AttributeNames.includes('imgAlt') && 
-                isStringValid(menuItem.imgAlt) && 
+                isStringAndNotEmpty(menuItem.imgAlt) && 
                 AttributeNames.includes('itemContent') && 
-                isStringValid(menuItem.itemContent)
+                isStringAndNotEmpty(menuItem.itemContent)
               );
       }
 
