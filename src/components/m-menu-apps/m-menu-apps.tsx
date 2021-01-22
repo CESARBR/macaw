f85a16ecd838da7a82d223a92e3c720c3f5d3ae4
filipe-torres <fvvtb@cesar.org.br>
2021-01-22 00:08:16 +0000

    Menu Apps - Validação do JSON e tratamento de erros

--- src/components/m-menu-apps/m-menu-apps.tsx
+++ src/components/m-menu-apps/m-menu-apps.tsx
@@ -30,9 +30,93 @@ export class MenuApps {
 
   render() {
     let menuData = this.menuItemsJson ? JSON.parse(this.menuItemsJson) : '',
-        mostUsedItems, commonItems, partnersItems;
+        mostUsedItems: Array<object>, commonItems: Array<object>, partnersItems: Array<object>;
 
     if(menuData) {
+      function isObject(objectItem: any) {
+        return objectItem.constructor === Object;
+      }
+
+      function isArray(arrayItem: any) {
+        return arrayItem instanceof Array;
+      }
+
+      function isStringValid(stringValue: any) {
+        return (typeof stringValue === 'string' && stringValue !== '');
+      }
+
+      function validateUrl(value: string) {
+        return (isStringValid(value) && /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value));
+      }
+
+      function menuItemIsCorrect(menuItem: any) {
+        return (menuItem.includes('link') && 
+                  validateUrl(menuItem.link) && 
+                  menuItem.includes('imgSrc') && 
+                  validateUrl(menuItem.imgSrc) && 
+                  menuItem.includes('imgAlt') && 
+                  isStringValid(menuItem.imgAlt) && 
+                  menuItem.includes('itemContent') && 
+                  isStringValid(menuItem.itemContent)
+                );
+      }
+
+      function isMenuBlockValid(menuBlock: any) {
+        if(!(isArray(menuBlock))) {
+          throw 'Dados do componente Menu Apps com estrutura incorreta: "' + menuBlock + '" deve ser um array.';
+        } else {
+          for(let menuItem of menuData.mostUsed) {
+            if(!(isObject(menuItem.constructor))) {
+              throw 'Dados do componente Menu Apps com estrutura incorreta: cada item do menu deve ser um objeto literal.';;
+            } else {
+              if(Object.keys(menuItem).length === 0) {
+                throw 'Dados do componente Menu Apps com estrutura incorreta: um item do menu não possui dados.';
+              } else if(!(menuItemIsCorrect(menuItem))) {
+                throw 'Dados do componente Menu Apps com estrutura incorreta: o item do menu não possui todos dados necessários.';
+              }
+            }
+          }
+        }
+      }
+
+      function checkMenuItemsJson() {
+        if(!(isObject(menuData))) {
+          throw 'Dados do componente Menu Apps com estrutura incorreta: "menuData" deve ser um objeto literal.';
+        } else if(Object.keys(menuData).length === 0) {
+          throw 'Dados do componente Menu Apps com estrutura incorreta: o objeto "menuData" está vazio.';
+        } else {
+          if(menuData.mostUsed) {
+            try {
+              isMenuBlockValid(menuData.mostUsed)
+            } catch (e) {
+              throw e;
+            }
+          }
+          if(menuData.common) {
+            try {
+              isMenuBlockValid(menuData.common)
+            } catch (e) {
+              throw e;
+            }
+          } 
+          if(menuData.partners) {
+            try {
+              isMenuBlockValid(menuData.partners)
+            } catch (e) {
+              throw e;
+            }
+          }
+        }
+      }
+
+      try {
+        checkMenuItemsJson();
+      }
+      catch (e) {
+        console.error('Verifique o formato do objeto de dados do menu apps. Para mais informações, consulte a ajuda do Macaw para o componente Menu Apps');
+        console.error(e);
+      }
+
       if(menuData.mostUsed) {
         mostUsedItems = menuData.mostUsed.map((item) =>
           <m-menu-apps-item link={item.link} img-src={item.imgSrc} img-alt={item.imgAlt}>{item.itemContent}</m-menu-apps-item>
